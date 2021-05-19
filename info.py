9d6c939ee6bca2dfd83547ef6a15a5a7f32f7773
ZainW <563680953@qq.com>
2021-05-19 17:38:23 +0800

    :tada finished

--- info.py
+++ info.py
@@ -1,9 +1,5 @@
-import jieba.posseg as pseg
-import matplotlib.pyplot as plt
-from os import path
-import sys
-import requests
-from cv2 import imread,imshow
+from requests import get
+from cv2 import imread
 from wordcloud import WordCloud,random_color_func
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
@@ -18,9 +14,7 @@ Urls = {
 def grab_info(site,lists):
     url = Urls[site]
     header = {'User-Agent': str(UserAgent().random)}   #Use fake headers
-    r = requests.get(url, headers=header)
-    # print(r.status_code)
-    # print(r.content.decode("utf-8"))
+    r = get(url, headers=header)
 
     soup = BeautifulSoup(r.content, 'lxml')
     parser = Parsers.get(site)
@@ -73,17 +67,7 @@ Parsers = {
 }
 
 def generat_img(freq):
-    #載入stopword
-    # stop_words = set(line.strip() for line in open('stopwords.txt', encoding='utf-8'))
-    # commentlist = comment_subjects
-    # for subject in comment_subjects:
-    #     if subject.isspace():continue
-    #     # segment words line by line
-    #     word_list = pseg.cut(subject)#分詞
-    #     for word, flag in word_list:
-    #         if not word in stop_words and flag == 'n':
-    #             commentlist.append(word)
-    
+
     mask_image = imread(r".\img\heart1.jpg")
     try:
         mask_image.shape
@@ -92,11 +76,8 @@ def generat_img(freq):
     # print(freq)
 
     wordcloud = WordCloud(font_path='simhei.ttf', background_color="#37414F",mask=mask_image, color_func=random_color_func).generate_from_frequencies(freq)
-    # Display the generated image:
-    # plt.imshow(wordcloud)
-    # plt.axis("off")
+
     wordcloud.to_file('./img/wordcloud.jpg')
-    # plt.show()
     return True
 
 
